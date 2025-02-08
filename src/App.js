d3eafac1534d36ca4e7e7d6f5957b7ce325728c2
Olawoyin <yustee2017@gmail.com>
2025-02-08 15:35:43 +0100

    Added the neccessary pages and comonent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import ScrollToTop from './ScrollToTop';
+import Home from './Pages/Home.jsx';
+import SharedLayout from './Components/SharedLayout.jsx'
+// import About from './Pages/About.jsx';
+// import Services from './Pages/Services.jsx';
+// import Contact from './Pages/Contact.jsx';
+
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <Router>
+      <ScrollToTop />
+      <Routes>
+
+        <Route path='/' element={<SharedLayout />}>
+          <Route index element={<Home />} />
+
+          {/* <Route path='about' element={<About />} />
+          <Route path='services' element={<Services />} />
+
+          <Route path='appointment' element={<Contact />} /> */}
+
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h3>hello World</h3>
+    </div>
+  )
+}
+
+export default Home
