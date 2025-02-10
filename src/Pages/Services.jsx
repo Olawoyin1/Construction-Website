37c9d841420a2095514d69d80448c6f31e72a225
Olawoyin <yustee2017@gmail.com>
2025-02-10 01:22:07 +0100

    Added whyus component to the services page

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -3,6 +3,7 @@ import ServiceCard from '../Components/ServiceCard'
 import Navbar from '../Components/Navbar'
 import FAQ from '../Components/FAQ'
 import ContactCard from '../Components/ContactCard'
+import WhyUs from '../Components/WhyUs'
 
 const Services = () => {
   return (
@@ -13,6 +14,7 @@ const Services = () => {
             <small className="muted">Transforming spaces with our comprehensive construction services.</small>
         </div>
         <ServiceCard />
+        <WhyUs />
         <FAQ />
         <ContactCard />
     </div>
